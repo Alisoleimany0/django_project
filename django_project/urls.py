e8eb65115da9e331c592e33f4684ee43291a95a6
ali <you@example.com>
2023-09-28 11:48:08 +0330

    fixed urls

--- django_project/urls.py
+++ django_project/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),  # Include the 'blog.urls' here
+    path('blog/', include('blog.urls')),
 ]
